9a7056c450c5a58c27ce2ba7175a605bf2418aab
Ears <gudyrmik@gmail.com>
2020-04-14 22:57:22 +0200

    Task 1

--- README.md
+++ README.md
@@ -22,3 +22,5 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+
+Entities list: Test, User, TestResult, Question (list), Answer(list)
--- config/application.rb
+++ config/application.rb
@@ -6,10 +6,12 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Testguru
+module TestGuru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.time_zone = 'Prague'
+    config.i18n.default_locale = :ru
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
