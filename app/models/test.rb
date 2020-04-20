a40c9619d613a9be54a2fce68f5c0eff19e5899e
Ears <gudyrmik@gmail.com>
2020-04-20 15:48:14 +0200

    Task 5 attempt 1

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,12 @@
 class Test < ApplicationRecord
+  has_one :category
+  has_many :questions
+  has_and_belongs_to_many :users
+
   def self.test_by_category(category)
-    joins("JOIN categories ON categories.id = category_id").where("categories.title = ?", category).order(title: 
-:desc).pluck(:title)
+    joins("JOIN categories ON categories.id = category_id")
+      .where(categories: { title: category })
+      .order(title: :desc)
+      .pluck(:title)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  has_and_belongs_to_many :tests
   def tests_by_level(level)
-    Test.joins("JOIN histories ON histories.user_id = ? AND tests.id = histories.test_id", id).where("tests.level = ?", level).uniq
+    Test.joins("JOIN histories ON tests.id = histories.test_id")
+      .where(tests: { level: level }, histories: { user_id: id })
+      .uniq
   end
 end
