1fa0f85e3d23c6c2591e6f3e386bbeb1c734dac6
Ears <gudyrmik@gmail.com>
2020-04-21 12:34:06 +0200

    Task 5 attempt 3

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,9 @@
 class User < ApplicationRecord
-  has_many :tests, dependent: :destroy
+  has_many :created_tests, dependent: :destroy, foreign_key: 'user_id', class_name: 'Test'
   has_many :histories, dependent: :destroy
   has_many :tests, through: :histories, dependent: :destroy
 
   def tests_by_level(level)
-    Test.joins(:histories)
-      .where(tests: { level: level }, histories: { user_id: id })
-      .uniq
+    created_tests.where({ level: level } )
   end
 end
