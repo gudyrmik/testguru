16310c98ca9d14372e9263530f565e42efff2f2c
Ears <gudyrmik@gmail.com>
2020-04-22 21:54:00 +0200

    Task 6 attempt 3

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -2,4 +2,11 @@ class Answer < ApplicationRecord
   belongs_to :question
 
   scope :correct, -> { where(correct: true) }
+
+  validate :answers_per_question, on: :create
+
+  def answers_per_question
+    answers_num = Question.find(question_id).answers.size
+    errors.add(:answers_max_exceeded, message: 'Question can\'t contain more than 4 answers.') if answers_num > 4
+  end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -3,12 +3,4 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
 
   validates :body, presence: :true
-  validate :validate_max_answers, on: [ :create, :update ]
-
-  private
-
-  def validate_max_answers
-    answers_num = answers.size
-    errors.add(:invalid_levels, message: 'Invalid number of levels') if answers_num > 4
-  end
 end
