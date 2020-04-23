bcd7c438690dd84e048367682f4caa225ae2a3fd
Ears <gudyrmik@gmail.com>
2020-04-23 11:14:55 +0200

    Task 6 final

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,7 +6,7 @@ class Answer < ApplicationRecord
   validate :answers_per_question, on: :create
 
   def answers_per_question
-    answers_num = Question.find(question_id).answers.size
-    errors.add(:answers_max_exceeded, message: 'Question can\'t contain more than 4 answers.') if answers_num > 4
+    answers_num = question.answers.count
+    errors.add(:answers_max_exceeded, message: 'Question can\'t contain more than 4 answers.') if answers_num >= 4
   end
 end
