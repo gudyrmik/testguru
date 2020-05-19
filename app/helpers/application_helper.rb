73df536432c4183fda01fe444d2f980615632590
Ears <gudyrmik@gmail.com>
2020-05-19 15:42:23 +0200

    final

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,6 @@
 module ApplicationHelper
 
-  def flash_key_to_bootstrap_class(key)
-    case key
-    when 'alert'
-      'alert-warning'
-    when 'notice'
-      'alert-info'
-    end
+  def flash_key_to_bootstrap_class(type)
+    { notice: 'alert-info', alert: 'alert-info' }[type.to_sym]
   end
 end
