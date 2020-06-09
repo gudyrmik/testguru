0bc11ccfbf00b4529d117cf166eccba4b7f4c6f1
Mikhail Gudyrin <gudyrmik@gmail.com>
2020-06-09 02:16:20 +0200

    qe

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "testguru_production"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: 'mysterious-sea-22657.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'polar-reef-80712.herokuapp.com/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
